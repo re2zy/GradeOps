2e22251bc6c160710410474cd30138cce8cc57e0
Janhvi Mittal <janhvimittal83@gmail.com>
2026-05-16 15:21:14 +0530

    models/__init__.py added

---
+++ backend/models/__init__.py
@@ -0,0 +1,11 @@
+from models.user import User, UserRole
+from models.exam import Exam, ExamStatus
+from models.submission import Submission, SubmissionStatus
+from models.grade import Grade, GradeStatus
+
+__all__ = [
+    "User", "UserRole",
+    "Exam", "ExamStatus",
+    "Submission", "SubmissionStatus",
+    "Grade", "GradeStatus",
+]
